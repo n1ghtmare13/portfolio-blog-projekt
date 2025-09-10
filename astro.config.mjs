888ee4b9b0b0827783b1bc86f48fd6873bfed5d9
n1ghtmare13 <mateusz.plecha@tm1.edu.pl>
2025-09-10 23:28:30 +0200

    chore: Enable prefetching in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: true, // Enable link prefetching for better performance
+});
